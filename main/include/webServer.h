46be87153fe8d8830a670f780ade952ca2498e93
as-moh <a.mohamma.nok@gmail.com>
2023-08-11 19:39:21 +0430

    clean var header

--- main/include/webServer.h
+++ main/include/webServer.h
@@ -15,9 +15,6 @@
 #include "driver/gpio.h"
 
 
-#define SSID    // "hwa"
-#define PASS     //"wifi1373"
-const char *TAG; //= "ESP32 Server";
 
 
 void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
@@ -28,9 +25,12 @@ esp_err_t on_handler(httpd_req_t *req);
 esp_err_t off_handler(httpd_req_t *req);
 esp_err_t get_handler_str(httpd_req_t *req);
 
+//httpd_uri_t uri_get;
+//httpd_uri_t uri_on;
+//httpd_uri_t uri_off;
+//httpd_uri_t uri_get_input;
 
-
-/* URI handler structure for GET /uri*/ 
+/* URI handler structure for GET /uri 
 httpd_uri_t uri_get = {
     .uri = "/",
     .method = HTTP_GET,
@@ -55,7 +55,7 @@ httpd_uri_t uri_get_input = {
     .method = HTTP_GET,
     .handler = get_handler_str,
     .user_ctx = NULL};
-
+*/
 
 httpd_handle_t start_webserver(void);
 void stop_webserver(httpd_handle_t server);
